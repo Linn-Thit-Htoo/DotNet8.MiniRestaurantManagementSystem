7d54e6db600d6f5262ccda4830e64334564a5a5c
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-07 22:51:29 +0630

    -

---
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/MenuItem/CreateMenuItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem
+{
+    public class CreateMenuItemDto
+    {
+        public string CategoryCode { get; set; } = null!;
+
+        public string MenuItemName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/MenuItem/MenuItemDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem
+{
+    public class MenuItemDto
+    {
+        public int MenuItemId { get; set; }
+
+        public string MenuItemCode { get; set; } = null!;
+
+        public string CategoryCode { get; set; } = null!;
+
+        public string MenuItemName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+    }
+}
