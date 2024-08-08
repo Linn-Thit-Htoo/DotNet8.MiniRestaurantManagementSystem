d8de44ae04196540d92bcedb0681706bc0e4d4d0
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:35:25 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Shared/Queries/OrderQuery.cs
+++ DotNet8.MiniRestaurantManagementSystem.Shared/Queries/OrderQuery.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Shared.Queries;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Shared.Queries
+public class OrderQuery
 {
-    public class OrderQuery
-    {
-        public static string OrderListQuery { get; } = @"SELECT O.OrderId, O.InvoiceNo,
+    public static string OrderListQuery { get; } =
+        @"SELECT O.OrderId, O.InvoiceNo,
 O.TotalPrice, O.CreatedDate, OD.OrderDetailId, OD.MenuItemCode, OD.Quantity, OD.TotalPrice
 FROM Tbl_Order O
 INNER JOIN Tbl_Order_Detail OD
 ON O.InvoiceNo = OD.InvoiceNo
 ORDER BY O.OrderId DESC";
-    }
 }
