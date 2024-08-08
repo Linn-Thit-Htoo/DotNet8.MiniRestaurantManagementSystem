220fb4fb8331e4c3ceba952144ab99292f4f8f96
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:33:00 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Category/CategoryDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Category/CategoryDto.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category
+public class CategoryDto
 {
-    public class CategoryDto
-    {
-        public int CategoryId { get; set; }
+    public int CategoryId { get; set; }
 
-        public string CategoryCode { get; set; } = null!;
+    public string CategoryCode { get; set; } = null!;
 
-        public string CategoryName { get; set; } = null!;
-    }
+    public string CategoryName { get; set; } = null!;
 }
--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Category/CreateCategoryDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Category/CreateCategoryDto.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category
+public class CreateCategoryDto
 {
-    public class CreateCategoryDto
-    {
-        public string CategoryName { get; set; } = null!;
-    }
+    public string CategoryName { get; set; } = null!;
 }
