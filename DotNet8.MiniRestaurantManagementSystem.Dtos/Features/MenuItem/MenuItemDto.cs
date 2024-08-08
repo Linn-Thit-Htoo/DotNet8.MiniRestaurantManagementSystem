2ef3a3c97a78630dc694783dc1265d5d71ab8da1
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:33:24 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/MenuItem/MenuItemDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/MenuItem/MenuItemDto.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem
+public class MenuItemDto
 {
-    public class MenuItemDto
-    {
-        public int MenuItemId { get; set; }
+    public int MenuItemId { get; set; }
 
-        public string MenuItemCode { get; set; } = null!;
+    public string MenuItemCode { get; set; } = null!;
 
-        public string CategoryCode { get; set; } = null!;
+    public string CategoryCode { get; set; } = null!;
 
-        public string MenuItemName { get; set; } = null!;
+    public string MenuItemName { get; set; } = null!;
 
-        public decimal Price { get; set; }
-    }
+    public decimal Price { get; set; }
 }
