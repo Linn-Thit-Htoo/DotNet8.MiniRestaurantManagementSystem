01b435058a2a1e34e68deae2920f48a8dd7113ba
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:29:06 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Api/DependencyInjection.cs
+++ DotNet8.MiniRestaurantManagementSystem.Api/DependencyInjection.cs
@@ -2,36 +2,47 @@
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+    public static IServiceCollection AddDependencyInjection(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        return services.AddDbContextService(builder)
+        return services
+            .AddDbContextService(builder)
             .AddDataAccessService()
             .AddBusinessLogicService()
             .AddCustomService();
     }
 
-    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        builder.Services.AddDbContext<AppDbContext>(opt =>
-        {
-            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+        builder.Services.AddDbContext<AppDbContext>(
+            opt =>
+            {
+                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+            },
+            ServiceLifetime.Transient,
+            ServiceLifetime.Transient
+        );
 
         return services;
     }
 
     private static IServiceCollection AddDataAccessService(this IServiceCollection services)
     {
-        return services.AddScoped<ICategoryService, CategoryService>()
+        return services
+            .AddScoped<ICategoryService, CategoryService>()
             .AddScoped<IMenuItemService, MenuItemService>()
             .AddScoped<IOrderService, OrderService>();
     }
 
     private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
     {
-        return services.AddScoped<BL_Category>()
-            .AddScoped<BL_MenuItem>();
+        return services.AddScoped<BL_Category>().AddScoped<BL_MenuItem>();
     }
 
     private static IServiceCollection AddCustomService(this IServiceCollection services)
