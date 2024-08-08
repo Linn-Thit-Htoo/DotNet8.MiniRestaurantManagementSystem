4ba83d866faef828125e87f90ae13dc9a0a200c4
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 14:21:04 +0630

    [ Added ] GetMenuItems Endpoint

---
+++ DotNet8.MiniRestaurantManagementSystem.Api/Controllers/MenuItemController.cs
@@ -0,0 +1,25 @@
+﻿using DotNet8.MiniRestaurantManagementSystem.Modules.Features.MenuItem;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.MiniRestaurantManagementSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : BaseController
+    {
+        private readonly IMenuItemService _menuItemService;
+
+        public MenuItemController(IMenuItemService menuItemService)
+        {
+            _menuItemService = menuItemService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems(CancellationToken cancellationToken)
+        {
+            var result = await _menuItemService.GetMenuItemsAsync(cancellationToken);
+            return Content(result);
+        }
+    }
+}
