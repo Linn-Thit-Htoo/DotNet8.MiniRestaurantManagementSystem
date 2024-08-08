07b5a1df2849625b8c29bed98c83f6ac91ec1af1
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:33:40 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Order/CreateOrderDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Order/CreateOrderDto.cs
@@ -1,15 +1,7 @@
-﻿using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Order;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Order
+public class CreateOrderDto
 {
-    public class CreateOrderDto
-    {
-        public decimal TotalPrice { get; set; }
-        public List<CreateOrderDetailDto> OrderDetails { get; set; }
-    }
+    public decimal TotalPrice { get; set; }
+    public List<CreateOrderDetailDto> OrderDetails { get; set; }
 }
--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Order/OrderDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/Order/OrderDto.cs
@@ -1,5 +1,4 @@
-﻿using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
