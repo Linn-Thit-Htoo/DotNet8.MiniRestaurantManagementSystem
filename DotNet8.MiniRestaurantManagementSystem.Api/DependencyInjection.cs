38cc39c1ab9f21895e5043863201a7cdc22c6136
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:29:00 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Api/DependencyInjection.cs
+++ DotNet8.MiniRestaurantManagementSystem.Api/DependencyInjection.cs
@@ -1,47 +1,41 @@
-﻿using DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
-using DotNet8.MiniRestaurantManagementSystem.Shared;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Api;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Api
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder)
-                .AddDataAccessService()
-                .AddBusinessLogicService()
-                .AddCustomService();
-        }
+        return services.AddDbContextService(builder)
+            .AddDataAccessService()
+            .AddBusinessLogicService()
+            .AddCustomService();
+    }
 
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(opt =>
         {
-            builder.Services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-                opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient, ServiceLifetime.Transient);
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
-        {
-            return services.AddScoped<ICategoryService, CategoryService>()
-                .AddScoped<IMenuItemService, MenuItemService>()
-                .AddScoped<IOrderService, OrderService>();
-        }
+    private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    {
+        return services.AddScoped<ICategoryService, CategoryService>()
+            .AddScoped<IMenuItemService, MenuItemService>()
+            .AddScoped<IOrderService, OrderService>();
+    }
 
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services.AddScoped<BL_Category>()
-                .AddScoped<BL_MenuItem>();
-        }
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_Category>()
+            .AddScoped<BL_MenuItem>();
+    }
 
-        private static IServiceCollection AddCustomService(this IServiceCollection services)
-        {
-            return services.AddScoped<DapperService>();
-        }
+    private static IServiceCollection AddCustomService(this IServiceCollection services)
+    {
+        return services.AddScoped<DapperService>();
     }
 }
--- DotNet8.MiniRestaurantManagementSystem.Api/GlobalUsings.cs
+++ DotNet8.MiniRestaurantManagementSystem.Api/GlobalUsings.cs
@@ -1,6 +1,7 @@
 // Global using directives
 
 global using DotNet8.MiniRestaurantManagementSystem.Api.Controllers.Base;
+global using DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
 global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category;
 global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem;
 global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Order;
@@ -8,4 +9,6 @@ global using DotNet8.MiniRestaurantManagementSystem.Extensions;
 global using DotNet8.MiniRestaurantManagementSystem.Modules.Features.Category;
 global using DotNet8.MiniRestaurantManagementSystem.Modules.Features.MenuItem;
 global using DotNet8.MiniRestaurantManagementSystem.Modules.Features.Order;
+global using DotNet8.MiniRestaurantManagementSystem.Shared;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
