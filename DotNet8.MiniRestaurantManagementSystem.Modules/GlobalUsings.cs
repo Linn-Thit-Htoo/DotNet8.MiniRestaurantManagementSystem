5664ad839ec2acc4dc1660c51c01b75d6b5a5f15
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:29:57 +0630

    [ Added ] Global Usings in Modules

---
+++ DotNet8.MiniRestaurantManagementSystem.Modules/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives
+
+global using System.Linq.Expressions;
+global using DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
+global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category;
+global using DotNet8.MiniRestaurantManagementSystem.Extensions;
+global using DotNet8.MiniRestaurantManagementSystem.Utils;
+global using Microsoft.EntityFrameworkCore;
