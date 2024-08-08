56af702b8829eb5e38f05a4f48266127993b993b
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:34:22 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/OrderDetail/CreateOrderDetailDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/OrderDetail/CreateOrderDetailDto.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail
+public class CreateOrderDetailDto
 {
-    public class CreateOrderDetailDto
-    {
-        public string MenuItemCode { get; set; } = null!;
+    public string MenuItemCode { get; set; } = null!;
 
-        public int Quantity { get; set; }
+    public int Quantity { get; set; }
 
-        public decimal TotalPrice { get; set; }
-    }
+    public decimal TotalPrice { get; set; }
 }
--- DotNet8.MiniRestaurantManagementSystem.Dtos/Features/OrderDetail/OrderDetailDto.cs
+++ DotNet8.MiniRestaurantManagementSystem.Dtos/Features/OrderDetail/OrderDetailDto.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail
+public class OrderDetailDto
 {
-    public class OrderDetailDto
-    {
-        public int OrderDetailId { get; set; }
+    public int OrderDetailId { get; set; }
 
-        public string InvoiceNo { get; set; } = null!;
+    public string InvoiceNo { get; set; } = null!;
 
-        public string MenuItemCode { get; set; } = null!;
+    public string MenuItemCode { get; set; } = null!;
 
-        public int Quantity { get; set; }
+    public int Quantity { get; set; }
 
-        public decimal TotalPrice { get; set; }
-    }
+    public decimal TotalPrice { get; set; }
 }
