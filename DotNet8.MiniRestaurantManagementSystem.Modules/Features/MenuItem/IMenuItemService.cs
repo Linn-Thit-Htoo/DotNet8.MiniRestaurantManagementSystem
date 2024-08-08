86d02cdcba1a4ca3b37b9957f680cc86d0f06932
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 12:35:40 +0630

    [ Added ] GetMenuItemsAsync Service

--- DotNet8.MiniRestaurantManagementSystem.Modules/Features/MenuItem/IMenuItemService.cs
+++ DotNet8.MiniRestaurantManagementSystem.Modules/Features/MenuItem/IMenuItemService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem;
+using DotNet8.MiniRestaurantManagementSystem.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace DotNet8.MiniRestaurantManagementSystem.Modules.Features.MenuItem
 {
     public interface IMenuItemService
     {
+        Task<Result<IEnumerable<MenuItemDto>>> GetMenuItemsAsync(CancellationToken cancellationToken);
     }
 }
--- DotNet8.MiniRestaurantManagementSystem.Modules/Features/MenuItem/MenuItemService.cs
+++ DotNet8.MiniRestaurantManagementSystem.Modules/Features/MenuItem/MenuItemService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
+using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem;
+using DotNet8.MiniRestaurantManagementSystem.Extensions;
+using DotNet8.MiniRestaurantManagementSystem.Utils;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,27 @@ namespace DotNet8.MiniRestaurantManagementSystem.Modules.Features.MenuItem
 {
     public class MenuItemService : IMenuItemService
     {
+        private readonly AppDbContext _context;
+
+        public MenuItemService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<IEnumerable<MenuItemDto>>> GetMenuItemsAsync(CancellationToken cancellationToken)
+        {
+            Result<IEnumerable<MenuItemDto>> result;
+            try
+            {
+                var lst = await _context.TblMenuItems.OrderByDescending(x => x.MenuItemId).ToListAsync();
+                result = Result<IEnumerable<MenuItemDto>>.Success(lst.Select(x => x.ToDto()));
+            }
+            catch (Exception ex)
+            {
+                result = Result<IEnumerable<MenuItemDto>>.Failure(ex);
+            }
+
+            return result;
+        }
     }
 }
