976bab18fc6b79292aacf8c2ca59172f2b02d868
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:35:42 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Shared/DapperService.cs
+++ DotNet8.MiniRestaurantManagementSystem.Shared/DapperService.cs
@@ -1,32 +1,22 @@
-﻿using Dapper;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Shared;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Shared
+public class DapperService
 {
-    public class DapperService
+    private readonly IConfiguration _configuration;
+
+    public DapperService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-
-        public DapperService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        public async Task<IEnumerable<T>> QueryAsync<T>(string query, object? parameters = null)
-        {
-            using IDbConnection db = GetSqlConnection();
-            var lst = await db.QueryAsync<T>(query, parameters);
-
-            return lst;
-        }
-
-        private SqlConnection GetSqlConnection() => new(_configuration.GetConnectionString("DbConnection"));
+        _configuration = configuration;
     }
+
+    public async Task<IEnumerable<T>> QueryAsync<T>(string query, object? parameters = null)
+    {
+        using IDbConnection db = GetSqlConnection();
+        var lst = await db.QueryAsync<T>(query, parameters);
+
+        return lst;
+    }
+
+    private SqlConnection GetSqlConnection() =>
+        new(_configuration.GetConnectionString("DbConnection"));
 }
