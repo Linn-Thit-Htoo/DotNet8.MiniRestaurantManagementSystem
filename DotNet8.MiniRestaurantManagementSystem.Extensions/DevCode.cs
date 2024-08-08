73e3f7ea253848e8a21f3e56abd94a10e47c838e
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:34:41 +0630

    [ Added ] Global Usings

--- DotNet8.MiniRestaurantManagementSystem.Extensions/DevCode.cs
+++ DotNet8.MiniRestaurantManagementSystem.Extensions/DevCode.cs
@@ -1,17 +1,9 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Extensions;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Extensions
+public static class DevCode
 {
-    public static class DevCode
+    public static string SerializeObject(this object obj)
     {
-        public static string SerializeObject(this object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
+        return JsonConvert.SerializeObject(obj);
     }
 }
