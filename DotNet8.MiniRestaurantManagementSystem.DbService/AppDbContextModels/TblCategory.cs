04974995d599c70378aa4da0c9352e7e84543508
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:32:35 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.DbService/AppDbContextModels/TblCategory.cs
+++ DotNet8.MiniRestaurantManagementSystem.DbService/AppDbContextModels/TblCategory.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
 
 public partial class TblCategory
 {
