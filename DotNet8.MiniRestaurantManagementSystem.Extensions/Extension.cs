b8341e505fec9237399ebce07d60d2d099113a2e
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-08 17:35:10 +0630

    Format Code

--- DotNet8.MiniRestaurantManagementSystem.Extensions/Extension.cs
+++ DotNet8.MiniRestaurantManagementSystem.Extensions/Extension.cs
@@ -1,91 +1,79 @@
-﻿using DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
-using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category;
-using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem;
-using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Order;
-using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DotNet8.MiniRestaurantManagementSystem.Extensions;
 
-namespace DotNet8.MiniRestaurantManagementSystem.Extensions
+public static class Extension
 {
-    public static class Extension
+    public static CategoryDto ToDto(this TblCategory dataModel)
     {
-        public static CategoryDto ToDto(this TblCategory dataModel)
+        return new CategoryDto
         {
-            return new CategoryDto
-            {
-                CategoryId = dataModel.CategoryId,
-                CategoryCode = dataModel.CategoryCode,
-                CategoryName = dataModel.CategoryName
-            };
-        }
+            CategoryId = dataModel.CategoryId,
+            CategoryCode = dataModel.CategoryCode,
+            CategoryName = dataModel.CategoryName
+        };
+    }
 
-        public static TblCategory ToEntity(this CreateCategoryDto categoryDto)
+    public static TblCategory ToEntity(this CreateCategoryDto categoryDto)
+    {
+        return new TblCategory
         {
-            return new TblCategory
-            {
-                CategoryCode = Ulid.NewUlid().ToString(),
-                CategoryName = categoryDto.CategoryName
-            };
-        }
+            CategoryCode = Ulid.NewUlid().ToString(),
+            CategoryName = categoryDto.CategoryName
+        };
+    }
 
-        public static TblMenuItem ToEntity(this CreateMenuItemDto menuItemDto)
+    public static TblMenuItem ToEntity(this CreateMenuItemDto menuItemDto)
+    {
+        return new TblMenuItem
         {
-            return new TblMenuItem
-            {
-                MenuItemCode = Ulid.NewUlid().ToString(),
-                CategoryCode = menuItemDto.CategoryCode,
-                MenuItemName = menuItemDto.MenuItemName,
-                Price = menuItemDto.Price
-            };
-        }
+            MenuItemCode = Ulid.NewUlid().ToString(),
+            CategoryCode = menuItemDto.CategoryCode,
+            MenuItemName = menuItemDto.MenuItemName,
+            Price = menuItemDto.Price
+        };
+    }
 
-        public static MenuItemDto ToDto(this TblMenuItem dataModel)
+    public static MenuItemDto ToDto(this TblMenuItem dataModel)
+    {
+        return new MenuItemDto
         {
-            return new MenuItemDto
-            {
-                MenuItemId = dataModel.MenuItemId,
-                MenuItemCode = dataModel.MenuItemCode,
-                CategoryCode = dataModel.CategoryCode,
-                MenuItemName = dataModel.MenuItemName,
-                Price = dataModel.Price
-            };
-        }
+            MenuItemId = dataModel.MenuItemId,
+            MenuItemCode = dataModel.MenuItemCode,
+            CategoryCode = dataModel.CategoryCode,
+            MenuItemName = dataModel.MenuItemName,
+            Price = dataModel.Price
+        };
+    }
 
-        public static TblOrder ToEntity(this CreateOrderDto orderDto)
+    public static TblOrder ToEntity(this CreateOrderDto orderDto)
+    {
+        return new TblOrder
         {
-            return new TblOrder
-            {
-                InvoiceNo = DateTime.Now.ToString("yyyMMddHHmmss"),
-                CreatedDate = DateTime.Now,
-                TotalPrice = orderDto.TotalPrice
-            };
-        }
+            InvoiceNo = DateTime.Now.ToString("yyyMMddHHmmss"),
+            CreatedDate = DateTime.Now,
+            TotalPrice = orderDto.TotalPrice
+        };
+    }
 
-        public static TblOrderDetail ToEntity(this CreateOrderDetailDto orderDetailDto, string invoice)
+    public static TblOrderDetail ToEntity(this CreateOrderDetailDto orderDetailDto, string invoice)
+    {
+        return new TblOrderDetail
         {
-            return new TblOrderDetail
-            {
-                InvoiceNo = invoice,
-                MenuItemCode = orderDetailDto.MenuItemCode,
-                Quantity = orderDetailDto.Quantity,
-                TotalPrice = orderDetailDto.TotalPrice
-            };
-        }
+            InvoiceNo = invoice,
+            MenuItemCode = orderDetailDto.MenuItemCode,
+            Quantity = orderDetailDto.Quantity,
+            TotalPrice = orderDetailDto.TotalPrice
+        };
+    }
 
-        public static OrderDetailDto ToDto(this TblOrderDetail dataModel)
+    public static OrderDetailDto ToDto(this TblOrderDetail dataModel)
+    {
+        return new OrderDetailDto
         {
-            return new OrderDetailDto
-            {
-                InvoiceNo = dataModel.InvoiceNo,
-                MenuItemCode = dataModel.MenuItemCode,
-                OrderDetailId = dataModel.OrderDetailId,
-                Quantity = dataModel.Quantity,
-                TotalPrice = dataModel.TotalPrice
-            };
-        }
+            InvoiceNo = dataModel.InvoiceNo,
+            MenuItemCode = dataModel.MenuItemCode,
+            OrderDetailId = dataModel.OrderDetailId,
+            Quantity = dataModel.Quantity,
+            TotalPrice = dataModel.TotalPrice
+        };
     }
 }
--- DotNet8.MiniRestaurantManagementSystem.Extensions/GlobalUsings.cs
+++ DotNet8.MiniRestaurantManagementSystem.Extensions/GlobalUsings.cs
@@ -1,3 +1,8 @@
 // Global using directives
 
+global using DotNet8.MiniRestaurantManagementSystem.DbService.AppDbContextModels;
+global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Category;
+global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.MenuItem;
+global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.Order;
+global using DotNet8.MiniRestaurantManagementSystem.Dtos.Features.OrderDetail;
 global using Newtonsoft.Json;
